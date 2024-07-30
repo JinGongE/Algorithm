c3f0c0914b8f0dbb58ba53fc10d637490e5f8b2e
JinGongE <88472788+JinGongE@users.noreply.github.com>
2024-07-30 10:36:37 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,11 @@
+#print(ord('a')-96, ord('z')-96)
+L = int(input())
+string = input()
+
+n=0
+hashing = 0
+for i in string:
+    alpha = ord(i)-96
+    hashing += alpha * (31 ** n)
+    n+=1
+print(hashing)
